353f849f5822aa347f00e464e4cd942411a5a08d
Sean Silva <silvas@purdue.edu>
2013-04-28 22:34:21 -0400

    Add simple example.

---
+++ examples/printMultiOutputs.js
@@ -0,0 +1,20 @@
+// Print out info about `build` declarations that have more than one output.
+// Reads input on stdin.
+// It prints out the rule name and the list of outputs.
+// E.g. print out info this one:
+//     build test.pdf test.aux test.log: latex test.tex
+// but not this one:
+//     build test.tex: weave $noweb_file
+
+var parser = require('../index.js');
+var p = parser();
+
+process.nextTick(function () {
+    process.stdin.pipe(p);
+});
+p.on('buildHead', function (o) {
+    if (o.outputs.length > 1) {
+        console.log(o.ruleName);
+        console.log(o.outputs);
+    }
+});
